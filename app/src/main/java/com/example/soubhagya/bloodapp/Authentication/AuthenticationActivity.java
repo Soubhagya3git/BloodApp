5646c08832ede1953f12bfefcd64330df7c65bac
soubhagya <soubhagya.nayak3.work@gmail.com>
2017-06-01 00:35:09 +0530

    firstTimeDetails ongoing...

--- app/src/main/java/com/example/soubhagya/bloodapp/Authentication/AuthenticationActivity.java
+++ app/src/main/java/com/example/soubhagya/bloodapp/Authentication/AuthenticationActivity.java
@@ -1,10 +1,11 @@
-package com.example.soubhagya.bloodapp;
+package com.example.soubhagya.bloodapp.Authentication;
 
 import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.soubhagya.bloodapp.R;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
