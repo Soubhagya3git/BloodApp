3a684aff585ee1ef73fd144f327c9b990e889106
soubhagya <soubhagya.nayak3.work@gmail.com>
2017-05-31 15:34:54 +0530

    authentication done

---
+++ app/src/main/java/com/example/soubhagya/bloodapp/AuthenticationActivity.java
@@ -0,0 +1,81 @@
+package com.example.soubhagya.bloodapp;
+
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ui.ResultCodes;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+
+        if (mAuth.getCurrentUser() != null) {
+            showSnackbar(R.string.already_logged_in);
+            // start MainActivity
+        }
+        else {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setTheme(R.style.GreenThemeFirebaseUI)
+                    .setIsSmartLockEnabled(false)
+                    .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                    .build(),
+                    RC_SIGN_IN
+            );
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // start MainActivity or QAActivity
+                //finish();
+                //return;
+                showSnackbar(R.string.login_success);
+            }
+            else {
+                if (response == null) {
+                    showSnackbar(R.string.sign_in_cancelled);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackbar(R.string.no_internet_connection);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackbar(R.string.unknown_error);
+                    return;
+                }
+            }
+        }
+    }
+
+    public void showSnackbar(int message) {
+        Snackbar.make(findViewById(R.id.activity_authentication_view), message, Snackbar.LENGTH_SHORT).show();
+    }
+
+}
